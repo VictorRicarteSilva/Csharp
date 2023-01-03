8f9d5249e4c95f70a5ba430395d6d840a8dee82b
Victor Ricarte <victorricarte155@gmail.com>
2023-01-02 21:50:43 -0300

    Initial commit

---
+++ Modulo1_Sintaxe/conceitosBasicos/conceitosBasicos/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using conceitosBasicos.Models;
+
+Console.WriteLine("Hello, World!");
+Pessoa p1 = new Pessoa();
+p1.Nome = "Victor";
+p1.Idade = 20;
+p1.Apresentar();
