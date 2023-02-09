e277ef55f1829302683dfb1f62a3fe6f5378e366
Victor Ricarte <victorricarte155@gmail.com>
2023-02-09 08:46:11 -0300

    add exercicio de for

---
+++ ExerciciosDeFor/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero;
+Console.WriteLine("Entrada:");
+numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Saida:");
+for (int i = 0; i < numero; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
