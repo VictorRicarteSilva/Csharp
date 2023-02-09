9c90fd858a14a0760debcc7021411c39e3244f23
Victor Ricarte <victorricarte155@gmail.com>
2023-02-09 10:58:16 -0300

    ExercicioTradicionalComClasses melhorado

--- ExercicioTradicionalComClasses/Classes/Triangulo.cs
+++ ExercicioTradicionalComClasses/Classes/Triangulo.cs
@@ -2,7 +2,19 @@ namespace ExercicioTradicionalComClasses.Classes;
 
 public class Triangulo
 {
+    // atributos
     public double A;
     public double B;
     public double C;
+
+    /// <summary>
+    /// Usando a fórmula de Heron para calcular a área de um triângulo
+    /// </summary>
+    /// <returns> double area</returns>
+    public double area()
+    {
+        double p = (A + B + C) / 2;
+        
+        return Double.Sqrt(p * (p - A) * (p - B) * (p - C));
+    }
 }
--- ExercicioTradicionalComClasses/Program.cs
+++ ExercicioTradicionalComClasses/Program.cs
@@ -6,26 +6,24 @@ Triangulo x, y;
 x = new Triangulo();
 y = new Triangulo();
 
-double areaX, areaY, pX, pY;
+double areaX, areaY;
 
 Console.WriteLine("Entre com as medidas do Triangulo X: ");
 x.A = Convert.ToDouble(Console.ReadLine());
 x.B = Convert.ToDouble(Console.ReadLine());
 x.C = Convert.ToDouble(Console.ReadLine());
 
-pX = (x.A + x.B + x.C) / 2;
-areaX = Double.Sqrt(pX * (pX - x.A) * (pX - x.B) * (pX - x.C));
+areaX = x.area();
 
 Console.WriteLine("Entre com as medidas do Triangulo Y: ");
 y.A = Convert.ToDouble(Console.ReadLine());
 y.B = Convert.ToDouble(Console.ReadLine());
 y.C = Convert.ToDouble(Console.ReadLine());
 
-pY = (y.A + y.B + y.C) / 2;
-areaY =  Double.Sqrt(pY * (pY - y.A) * (pY - y.B) * (pY - y.C));
+areaY = y.area();
 
-Console.WriteLine($"Area do triangulo X = {areaX.ToString("F4")}");
-Console.WriteLine($"Area do triangulo Y = {areaY.ToString("F4")}");
+Console.WriteLine($"Area do triangulo X = {areaX.ToString("F")}");
+Console.WriteLine($"Area do triangulo Y = {areaY.ToString("F")}");
 
 if (areaX > areaY)
 {
